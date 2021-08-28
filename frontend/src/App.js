553434bf1ff2c66e177621dd4c1e827a6a7294ea
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-08-28 10:42:37 -0300

    estado para titulos e conteudos dos blocos de notas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Aside from './Components/Aside';
 import NoteList from './Components/NoteList';
-import "./style/app.css";
-import "./style/global.css";
+import './style/app.css';
+import './style/global.css';
 
 const App = () => {
+  const [title, setTitle] = React.useState('');
+  const [notes, setNotes] = React.useState('');
+
   return (
     <div id="app">
-      <Aside />
-      <NoteList />
+      <Aside
+        title={title}
+        setTitle={setTitle}
+        notes={notes}
+        setNotes={setNotes}
+      />
+      <NoteList title={title} notes={notes} />
     </div>
   );
 };
