eacca9fd33249194fb472c5bca1d016df0e3d577
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-08-28 10:46:17 -0300

    camos de titulo e anotacao reativos, post de nova anotacao no banco de dados

--- frontend/src/Components/Aside.js
+++ frontend/src/Components/Aside.js
@@ -1,22 +1,45 @@
 import React from 'react';
+import api from '../Services/api';
 import '../style/aside.css';
 
-const Aside = () => {
+const Aside = ({ title, setTitle, notes, setNotes }) => {
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/annotations', {
+      title,
+      notes,
+      priority: false,
+    });
+
+    if (response.status === 200) {
+      setTitle('');
+      setNotes('');
+    }
+  }
+
   return (
     <aside>
       <strong>Caderno de Notas</strong>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-block">
           <label htmlFor="titulo">Titulo da Anotação</label>
-          <input id="titulo" />
+          <input
+            id="titulo"
+            required
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
         </div>
 
         <div className="input-block">
           <label htmlFor="nota">Anotações</label>
           <textarea
             id="nota"
-            defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          ></textarea>
+            required
+            value={notes}
+            onChange={({ target }) => setNotes(target.value)}
+          />
         </div>
 
         <button type="submit">Salvar</button>
