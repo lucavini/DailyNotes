3de9941c40290b0ae9b1e167ff18ca49bd9c3b93
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-09-01 16:14:37 -0300

    função de save automatico para edição de cards, botões de deletar e alterar prioridade

--- frontend/src/Components/NoteCard.js
+++ frontend/src/Components/NoteCard.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { AiTwotoneDelete, AiFillExclamationCircle } from 'react-icons/ai';
+import api from '../Services/api';
+const NoteCard = ({ note, handleDelete, handleChangePriority }) => {
+  const [changedNotes, setChangedNotes] = React.useState(note.notes);
 
-const NoteCard = ({note}) => {
+  async function handleSave(notes, value) {
+    if (value !== notes) {
+      await api.post(`/contents/${note._id}`, {
+        notes: value,
+      });
+    }
+  }
+
+  function handleChange(target, notes) {
+    setChangedNotes(target.value);
+    handleSave(notes, target.value);
+  }
+
   return (
     <React.Fragment>
       <div>
         <strong>{note.title}</strong>
-        <div>X</div>
+        <div onClick={() => handleDelete(note._id)}>
+          <AiTwotoneDelete />
+        </div>
       </div>
 
-      <textarea defaultValue={note.notes}></textarea>
+      <textarea
+        defaultValue={changedNotes}
+        onChange={({ target }) => handleChange(target, note.notes)}
+      />
 
-      <span>!</span>
+      <span
+        className="setPriority"
+        onClick={() => handleChangePriority(note._id)}
+      >
+        <AiFillExclamationCircle />
+      </span>
     </React.Fragment>
   );
 };
